ee8a67ec775d476642f0d02a986e7f1a9225c42b
m-w-hatano <89900146+m-w-hatano@users.noreply.github.com>
2022-02-16 19:39:49 +0900

    バブルチャート機能追加

--- quest_dashbord.py
+++ quest_dashbord.py
@@ -1,13 +1,16 @@
+from ast import Index
 import numpy as np
 import pandas as pd
 import streamlit as st
 import pydeck as pdk
 import plotly.express as px
+# import base64
 
 
 st.set_page_config(layout="wide")
 st.title('Team C')
 st.header('インタラクティブダッシュボード')
+st.subheader('110万行計算するので表示に2～3分かかります(T*T)')
 
 df_cate_names = pd.read_csv('./quest22_data/category_names.csv')
 # df_cate_names
@@ -19,9 +22,16 @@ df_sales1 = pd.read_csv('./quest22_data/sales_history1.csv', encoding='shift_jis
 df_sales2 = pd.read_csv('./quest22_data/sales_history2.csv', encoding='shift_jis')
 df_sales1_2 = pd.concat([df_sales1, df_sales2])
 df_sales1_2 = pd.merge(df_sales1_2, df_item_cate_name, on='商品ID')
-# df_sales1_2
+df_date_category = pd.read_csv('./quest22_data/molding_data.csv')
 
-#--------------------------
+ # 使わない諸々を解放
+del df_cate_names
+del df_item_cate
+del df_sales1
+del df_sales2
+del df_item_cate_name
+
+#--------------集計前データ--------------------------
 # date型に変換DatetimeIndexに変換して集計しやすく
 df_sales1_2['日付'] = pd.to_datetime(df_sales1_2['日付'])
 df_sales1_2 = df_sales1_2.set_index(df_sales1_2["日付"])
@@ -29,7 +39,34 @@ df_sales1_2 = df_sales1_2.set_index(df_sales1_2["日付"])
 #売上金額をカラムに追加
 df_sales1_2['売上金額'] = df_sales1_2['商品価格'] * df_sales1_2['売上個数']
 #全データの確認用
-df_sales1_2
+st.subheader('■全データ表示')
+# df_sales1_2
+
+st.subheader('■カテゴリー別に集計 ※サークルの大きさは商品単価平均')
+# df_date_category
+
+
+
+#バブルチャート
+fig = px.scatter(df_date_category,
+                x='総売上個数',
+                y='総売上金額',
+                range_x=[0,600],
+                range_y=[1000,500000],
+                size="単価平均",
+	            size_max = 50,
+                color="商品カテゴリ名",
+                animation_frame='日付',
+                animation_group='商品カテゴリ名',
+                width=800,
+                height=800)
+
+st.plotly_chart(fig)
+
+# csv = df_sales1_2.to_csv(index=False) 
+# b64 = base64.b64encode(csv.encode()).decode()
+# href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8.csv">Download Link</a>'
+# st.markdown(f"CSVファイルのダウンロード(utf-8):  {href}", unsafe_allow_html=True)
 
 
 #フィルタリング機能---------------------------
@@ -43,7 +80,7 @@ select_category = st.sidebar.multiselect("カテゴリでフィルタ",category_
 df_sales1_2 = df_sales1_2[(df_sales1_2['商品カテゴリ名'].isin(select_category))]
 
 # 集計時間単位の切り替え
-date_span = st.sidebar.radio("集計範囲切り替え　※W=週 M=月 Q=四半期", ("W","M", "Q","Y"))
+date_span = st.sidebar.radio("集計期間切り替え　※W=週 M=月 Q=四半期", ("W","M", "Q","Y"))
 
 #  -------------------------
 
@@ -63,6 +100,12 @@ df_quant_line = shukei['売上個数']
 st.line_chart(df_quant_line)
 df_sales1_2
 
+#CSV出力機能
+# csv2 = df_sales1_2.to_csv(index=False) 
+# b64 = base64.b64encode(csv.encode()).decode()
+# href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8.csv">Download Link</a>'
+# st.markdown(f"CSVファイルのダウンロード(utf-8):  {href}", unsafe_allow_html=True)
+
 
 st.header("商品ID別売上推移") 
 productID_list =  df_sales1_2['商品ID'].unique()
@@ -73,3 +116,5 @@ productID_Quant_line = productID_shukei['売上個数']
 st.line_chart(productID_Quant_line)
 df_product_sales 
 productID_shukei[['売上個数','売上金額']]
+
+
